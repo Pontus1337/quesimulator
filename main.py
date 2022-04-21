e96ef115ae06cd66726afa59e7a98e75d348aee8
Pontus1337 <lindhpontus1@gmail.com>
2022-04-21 09:22:04 +0200

    Started making statisitcs for the simulator

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from random import randint, random
 
+
 class Store:
     def __init__(self,hour:int,minute:int,line:list) -> None:
         self.hour=hour
@@ -22,13 +23,13 @@ class Store:
         self.line.append(
         Person(f"Person {n}",Task()))
         if self.minute<10 and self.hour<10:
-               print(f"{self.line[-1].name} arrived at the time 0{self.hour}:0{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}")
+               print(f"{self.line[-1].name} arrived at the time 0{self.hour}:0{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}\n")
         elif self.hour<10: 
-                print(f"{self.line[-1].name} arrived at the time 0{self.hour}:{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}")
+                print(f"{self.line[-1].name} arrived at the time 0{self.hour}:{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}\n")
         elif self.minute<10:
-               print(f"{self.line[-1].name} arrived at the time {self.hour}:0{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}") 
+               print(f"{self.line[-1].name} arrived at the time {self.hour}:0{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}\n") 
         else:
-                print(f"{self.line[-1].name} arrived at the time {self.hour}:{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}")
+                print(f"{self.line[-1].name} arrived at the time {self.hour}:{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}\n")
 
 
 class Person:
@@ -52,20 +53,33 @@ def Task():
 def main():
     h=0
     n=0
+    waiting=0
     store1=Store(9,0,[])
-    while store1.hour<18:
+    while store1.hour<18 or len(store1.line)>0:
         store1.New_Time()
-        if randint(0,100)>20:
+
+        if randint(0,100)<=20 and not store1.hour>=18:
             n+=1
             store1.Add_To_Line(n)
 
         if h==0 and len(store1.line)>0:
             h=store1.line[0].tasks
+            t=store1.line[0].name
+            h-=0.5
             del store1.line[0]
+        elif h>0:
             h-=0.5
+            if h==0:
+                print(f"{t} removed from queue at {store1.hour}:{store1.minute}\n")                
+                
+        print(len(store1.line))
+        waiting+=len(store1.line)
+    waiting_per_consumer=round(waiting*60/n)
+    statistics=f"{n} consumers vere served, total waiting time was {waiting} minutes long = {waiting_per_consumer} seconds per consumer "
+    print (statistics)
 
-        else:
-            h-=0.5
+        
+
 
 
 
