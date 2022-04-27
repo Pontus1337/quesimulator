5992cd99566553fff0bcd3fbf3afdc1a4dbca339
Pontus1337 <lindhpontus1@gmail.com>
2022-04-27 15:22:37 +0200

    I have imprivoed upon the queueing function

--- main.py
+++ main.py
@@ -1,91 +1,83 @@
-
-
-from random import randint, random
-
+from random import randint, random, uniform
 
 class Store:
-    def __init__(self,hour:int,minute:int,line:list) -> None:
+    def __init__(self,hour:int,minute:int,line:list,help:bool) -> None:
         self.hour=hour
         self.minute=minute
         self.line=line
+        self.help=help
 
     def New_Time(self):
         # The time counter
         self.minute+=1
         if self.minute%60==0:
             self.hour+=1
-            self.minute=0
-
-            
+            self.minute=0      
 
     # Tells whenever a new peroson has entered the queue, the time they entered queue and how many tasks they need help with
-    def Add_To_Line(self,n):
-        self.line.append(
-        Person(f"Person {n}",Task()))
+    def Add_To_Line(self,n,):
+        self.line.append(Person(f"Person {n}",Task(),0))
         if self.minute<10 and self.hour<10:
-               print(f"{self.line[-1].name} arrived at the time 0{self.hour}:0{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}\n")
+            print(f"{self.line[-1].name} arrived at the time 0{self.hour}:0{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}\n")
         elif self.hour<10: 
                 print(f"{self.line[-1].name} arrived at the time 0{self.hour}:{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}\n")
         elif self.minute<10:
-               print(f"{self.line[-1].name} arrived at the time {self.hour}:0{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}\n") 
+            print(f"{self.line[-1].name} arrived at the time {self.hour}:0{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}\n") 
         else:
                 print(f"{self.line[-1].name} arrived at the time {self.hour}:{self.minute} with {self.line[-1].tasks} tasks in place {len(self.line)}\n")
 
+    # This is the queue function deciding how thr queue works
+    def thequeue(self):
+        if len(self.line)>0:
+            if self.help == False:
+                self.line[0].tasks-=0.5
+                self.help= True
+            elif self.line[0].tasks>0:
+                self.line[0].tasks-=0.5
+            elif self.line[0].tasks==0:
+                if len(self.line)>1:
+                    print(f"{self.line[0].name} walked out at {self.hour}:{self.minute} and {self.line[1].name} starts getting help")
+                else:
+                    print(f"{self.line[0].name} walked out at {self.hour}:{self.minute}")
+                    self.help=False
+                del self.line[0]
 
 class Person:
-    def __init__(self,name:str,tasks:int) -> None:
+    def __init__(self,name:str,tasks:int,waited:int) -> None:
         self.name=name
         self.tasks=tasks
+        self.waited=waited
 
 def Task():
     n=1
-    rand=random()
-    while rand<1/2**n:
+    # rand=random()
+    rand = round(uniform(0.01, 1), 2)
+    print(rand)
+    while rand<1/(2**n):
         n+=1
     return n
-    
-
-
-
-
-
 
 def main():
-    h=0
-    n=0
+
+    person=0
     waiting=0
-    store1=Store(9,0,[])
+    store1=Store(9,0,[],False)
+
     while store1.hour<18 or len(store1.line)>0:
         store1.New_Time()
 
         if randint(0,100)<=20 and not store1.hour>=18:
-            n+=1
-            store1.Add_To_Line(n)
+            person+=1
+            store1.Add_To_Line(person)
 
-        if h==0 and len(store1.line)>0:
-            h=store1.line[0].tasks
-            t=store1.line[0].name
-            h-=0.5
-            del store1.line[0]
-        elif h>0:
-            h-=0.5
-            if h==0:
-                print(f"{t} removed from queue at {store1.hour}:{store1.minute}\n")                
+        store1.thequeue()
+        if store1.help==True:
+            waiting+=len(store1.line)-1
                 
-        print(len(store1.line))
-        waiting+=len(store1.line)
-    waiting_per_consumer=round(waiting*60/n)
-    statistics=f"{n} consumers vere served, total waiting time was {waiting} minutes long = {waiting_per_consumer} seconds per consumer "
+    waiting_per_consumer=round(waiting*60/person)
+    statistics=f"{person} consumers vere served, total waiting time was {waiting} minutes long = {waiting_per_consumer} seconds per consumer "
     print (statistics)
 
-        
-
-
-
-
-
-
-        
 
 if __name__=="__main__":
     main()
